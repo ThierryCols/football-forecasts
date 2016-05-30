5d35c38f43ef6c07872eff8bd9548b500079e84f
ThierryCols <thierrycolsenet@gmail.com>
2016-05-30 18:32:54 +0200

    better js

--- src/components/Bet.jsx
+++ src/components/Bet.jsx
@@ -1,32 +1,30 @@
 import React from 'react';
 
 export default React.createClass({
-  getGames: function() {
+  getGames() {
     return this.props.games || [];
   },
   getTeamName: function(teamId) {
     return this.props.teams[teamId].teamName;
   },
-  displayGame: function(game) {
-    return <div className={'game'.concat(game.id)}>
+  displayGame(game) {
+    return <div className={`game${game.id}`}>
       <h4>Game {game.id}</h4>
       <p>{this.getTeamName(game.teamId1)} - {this.getTeamName(game.teamId2)}</p>
       <div className="pronos">
         <input type="number"
-          placeholder={'Score for '.concat(this.getTeamName(game.teamId1))}
-          name={'game'.concat(game.id).concat('ScoreTeam1')} />
+          placeholder={`Score for ${this.getTeamName(game.teamId1)}`}
+          name={`game${game.id}ScoreTeam1`} />
          -
         <input type="number"
-          placeholder={'Score for '.concat(this.getTeamName(game.teamId2))}
-          name={'game'.concat(game.id).concat('ScoreTeam2')} />
+          placeholder={`Score for ${this.getTeamName(game.teamId2)}`}
+          name={`game${game.id}ScoreTeam2`} />
       </div>
     </div>
   },
-  render: function() {
+  render() {
     return <div className="bet">
-      {this.getGames().map(game =>
-        this.displayGame(game)
-      )}
+      {this.getGames().map(this.displayGame)}
     </div>;
   }
 });

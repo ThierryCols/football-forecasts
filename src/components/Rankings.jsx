c0fb889783cf2fbe37c502c94f6a4ba0996d3c6b
ThierryCols <thierrycolsenet@gmail.com>
2016-06-04 08:33:32 +0100

    add rankigns component

--- src/components/Rankings.jsx
+++ src/components/Rankings.jsx
@@ -4,18 +4,40 @@ class Rankings extends React.Component {
   getUsers() {
     return this.props.users;
   }
-  displayUserScore() {
 
+  getUserScore(user) {
+    const userPronos = this.props.pronos.filter((prono) => prono.userId === user.id);
+
+    const finalScore = userPronos.reduce((score, prono) => {
+      if (this.props.results[prono.gameId] &&
+      this.props.results[prono.gameId].teamId1 === prono.teamId1 &&
+      this.props.results[prono.gameId].teamId2 === prono.teamId2) {
+        return score + 3;
+      } else if (this.props.results[prono.gameId] &&
+      this.props.results[prono.gameId].winner === prono.winner) {
+        return score + 1;
+      }
+      return score;
+    }, 0);
+    return finalScore;
+  }
+
+  displayUserScore(user) {
+    return (<ul key={user.id}>
+      {user.name} --- score: {::this.getUserScore(user)}
+    </ul>);
   }
   render() {
     return (<div className="rankings">
-      {this.getUsers().map(this.displayUserScore)}
+      {this.getUsers().map(::this.displayUserScore)}
     </div>);
   }
 }
 
 Rankings.propTypes = {
   users: PropTypes.array,
+  pronos: PropTypes.array,
+  results: PropTypes.object,
 };
 
 export default Rankings;
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Bet from './components/Bet';
+// import Bet from './components/Bet';
+import Rankings from './components/Rankings';
 
 // temporary for dev matters
 const data = {
@@ -19,6 +20,21 @@ const data = {
     userId: 1,
     teamId1: 2,
     teamId2: 1,
+    winner: 1,
+  }, {
+    id: 2,
+    gameId: 1,
+    userId: 1,
+    teamId1: 3,
+    teamId2: 0,
+    winner: 1,
+  }, {
+    id: 3,
+    gameId: 1,
+    userId: 2,
+    teamId1: 2,
+    teamId2: 3,
+    winner: 2,
   }],
   games: [{
     id: 1,
@@ -31,6 +47,13 @@ const data = {
     teamId2: 1,
     gameDate: '2016-06-20',
   }],
+  results: {
+    1: {
+      teamId1: 2,
+      teamId2: 1,
+      winner: 1,
+    },
+  },
   activeUser: 1,
   teams: {
     1: {
@@ -46,11 +69,16 @@ const data = {
 
 
 ReactDOM.render(
-  <Bet
-    activeUser={data.activeUser}
-    pronos={data.pronos.filter((pron) => pron.userId === data.activeUser)}
-    games={data.games}
-    teams={data.teams}
+  // <Bet
+  //   activeUser={data.activeUser}
+  //   pronos={data.pronos.filter((pron) => pron.userId === data.activeUser)}
+  //   games={data.games}
+  //   teams={data.teams}
+  // />,
+  <Rankings
+    users={data.users}
+    pronos={data.pronos}
+    results={data.results}
   />,
   document.getElementById('app')
 );
